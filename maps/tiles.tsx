54196c2c3840b31456929b1384a4c11b80fff164
calhanna <86635307+calhanna@users.noreply.github.com>
2021-08-05 14:51:50 +1200

    Added new level and tilesheet files

---
+++ maps/tiles.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Tiles" tilewidth="128" tileheight="128" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../images/tiles/girder.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../images/tiles/anchor.png"/>
+ </tile>
+</tileset>
