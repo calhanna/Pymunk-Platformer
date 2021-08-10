7d3a44dcfee259ab1c6bd8777ef698ad13e26536
calhanna <86635307+calhanna@users.noreply.github.com>
2021-08-10 18:06:19 +1200

    New "tutorial" level, ladders and small platforms
    Ladders and small platforms have been added to test_level as well

--- maps/tiles.tsx
+++ maps/tiles.tsx
@@ -1,10 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="Tiles" tilewidth="128" tileheight="128" tilecount="2" columns="0">
+<tileset name="Tiles" tilewidth="128" tileheight="128" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="128" height="128" source="../images/tiles/girder.png"/>
  </tile>
  <tile id="1">
   <image width="128" height="128" source="../images/tiles/anchor.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../images/tiles/metal_tile.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../images/tiles/ladder.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="64" source="../images/tiles/metal platform (sm).png"/>
  </tile>
 </tileset>
